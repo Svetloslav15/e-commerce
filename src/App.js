5d4658c6243a7a98f45ac77d7f89bb22865ddd3d
Svetloslav15 <sn_novoselski@abv.bg>
2019-12-25 22:00:32 +0200

    feat(Header): add header with navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './Components/header/header';
 import HomePage from './pages/home-page/home-page';
 import ShopPage from "./pages/shop-page/shop-page";
 
 const App = () => {
   return (
     <div>
+        <Header/>
         <Switch>
             <Route exact path='/' component={HomePage}/>
             <Route path='/shop' component={ShopPage}/>
