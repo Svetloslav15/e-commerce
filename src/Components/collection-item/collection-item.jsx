2f19452af56e6f573aa68b0b7c1fbe06c2d32ef4
Svetloslav15 <sn_novoselski@abv.bg>
2019-12-28 19:47:37 +0200

    feat(Cart): add functionality for adding products to cart

--- src/Components/collection-item/collection-item.jsx
+++ src/Components/collection-item/collection-item.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import './collection-item.scss';
+import CustomButton from '../custom-button/custom-button';
+import {addItemToCart} from "../../redux/cart/cart-actions";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
-    return(
+const CollectionItem = ({item, addItemToCart}) => {
+    const {id, name, price, imageUrl} = item;
+    return (
         <div className="collection-item">
             <div className="image" style={{backgroundImage: `url(${imageUrl})`}}/>
             <div className="collection-footer">
                 <span className="name">{name}</span>
-                <span className="price">$ {price}</span>
+                <span className="price">${price}</span>
             </div>
+            <CustomButton inverted onClick={() => addItemToCart(item)}>Add to Cart</CustomButton>
         </div>
     )
 };
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+    addItemToCart: (item) => dispatch(addItemToCart(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
