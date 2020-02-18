79da3bcd0239e8e1ab575a56362e6fc37d979abc
Svetloslav15 <sn_novoselski@abv.bg>
2020-02-18 07:33:20 +0200

    git add .

--- src/Components/custom-button/custom-button.jsx
+++ src/Components/custom-button/custom-button.jsx
@@ -3,8 +3,8 @@ import './custom-button.scss';
 
 const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => (
     <button className={`${inverted ? 'inverted' : ''}
-     ${isGoogleSignIn ? 'google-sign-in' : ''}
-      custom-button`} {...otherProps}>
+        ${isGoogleSignIn ? 'google-sign-in' : ''}
+        custom-button`} {...otherProps}>
         {children.toUpperCase()}
     </button>
 );
